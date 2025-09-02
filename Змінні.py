fb8f83c0fc290055f52ce37fe9ed5eeb004ed9c4
Andriy-16 <andrijsavko652@gmail.com>
2025-09-02 22:33:10 +0300

    Add files via upload

---
+++ Змінні.py
@@ -0,0 +1,16 @@
+vik = 16
+shyslo = 3.14
+pryvitania = "Привіт"
+student = True
+koordinaty = (1,2,3)
+unikalni_koordinaty = {1,2,3}
+print(vik,type (vik))
+print(shyslo,type (shyslo))
+print(pryvitania,type (pryvitania))
+print(student,type (student))
+print(koordinaty,type (koordinaty))
+print(unikalni_koordinaty,type (unikalni_koordinaty))
+
+
+
+
